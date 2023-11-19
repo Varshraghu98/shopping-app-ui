e49e07a046d8af0fb2fecd1891b0bb6c7cb4b907
Varshini Raghunath <varshiniraghunath@Varshinis-Air.fritz.box>
2023-11-19 13:43:02 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent,
+    ForbiddenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
